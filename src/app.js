c3685a87f322286bfddca9c129375f59ade8c734
skeerti <srinagesh_keerti@intuit.com>
2021-05-18 16:10:45 +0530

    Second commit of weather app project

--- src/app.js
+++ src/app.js
@@ -5,17 +5,14 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const app = express()
 
-// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
